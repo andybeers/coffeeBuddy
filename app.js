d14155375fdd39b500c47ddd7ed020a4a41c0efa
Andrew Beers <andybeers@gmail.com>
2016-09-06 17:43:36 -0700

    Updated calculate brew. Fixed col spacing. added recipe object

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var brewButton = document.getElementById("getBrewing");
 var strongerButton = document.getElementById("stronger");
 var weakerButton = document.getElementById("lighter");
+var saveButton = document.getElementById("save");
 
 
 function calculateBrew() {
@@ -9,16 +10,40 @@ function calculateBrew() {
 	console.log(oz + " oz input");
 	var gWater = oz * 29.57; //convert fluid oz to grams
 	console.log(gWater + " grams of water");
-	var gCoffee = gWater/ratio; //calculate grams coffee
+	var gCoffee = Math.round(gWater/ratio); //calculate grams coffee
 	console.log(gCoffee);
-	var brewWater = gCoffee * 2 + gWater; //calculate grams water accounting for grounds absorption
+	var brewWater = Math.round(gCoffee * 2 + gWater); //calculate grams water accounting for grounds absorption
 	console.log(brewWater);
-	var recipe = [oz, gCoffee, brewWater, ratio];
-	console.log(recipe);
-	return recipe;
+	var calcRecipe = [oz, gCoffee, brewWater, ratio];
+	console.log(calcRecipe);
+	document.getElementById("outputCoffee").textContent = calcRecipe[1];
+	document.getElementById("outputWater").textContent = calcRecipe[2];
+	return calcRecipe;
 }
 
 brewButton.addEventListener("click", calculateBrew);
+
+function Recipe(name, ounces, gramsCoffee, gramsWater, ratioWater) {
+	this.name = name;
+	this.ounces = ounces;
+	this.gramsCoffee = gramsCoffee;
+	this.gramsWater = gramsWater;
+	this.ratioWater = ratioWater;
+}
+
+// function writeRecipe(outputId, recipeArray) {
+
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 
