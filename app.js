e53da07f065c5ce4168f828c0216fd7e0dcc4fce
Andrew Beers <andybeers@gmail.com>
2016-09-08 13:54:15 -0700

    Added extensive comments to app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var brewButton = document.getElementById("getBrewing");
+var brewButton = document.getElementById("getBrewing"); //Grab all buttons and inputs and store in variables
 var strongerButton = document.getElementById("stronger");
 var lighterButton = document.getElementById("lighter");
 var saveButton = document.getElementById("save");
 var nameField = document.getElementById("nameInput");
 var selectDrop = document.getElementById("savedRec");
-var recipeOutput = [];
+var recipeOutput = []; //Create array to store recipe values
 
+//Function to calculate grams of coffee and water given an ounce input
 function calculateBrew() {
 	var ratioNumber = document.getElementById("ratio").textContent; //Grab ratio from page. Default is 16:1
-	var oz = parseInt(document.getElementById("ozInput").value); //grab user input value for oz
+	var oz = parseInt(document.getElementById("ozInput").value); //Grab user input value for oz
 	console.log(oz + " oz input");
-	if (isNaN(oz)) {
+	if (isNaN(oz)) { //Make sure oz input isn't empty or invalid
 		alert("Please enter an ounce number.");
 	} else {
 		var finalWater = oz * 29.57; //convert fluid oz to grams
@@ -23,31 +24,37 @@ function calculateBrew() {
 		console.log(calcRecipe);
 		document.getElementById("outputCoffee").textContent = calcRecipe[1]; //write coffee output to page
 		document.getElementById("outputWater").textContent = calcRecipe[2]; //write water output to page
-		recipeOutput = calcRecipe;
+		recipeOutput = calcRecipe; //store recipe in global variable as an array
 	}
 }
 
+//Function to increase ("widen") brew ratio
 function makeLighter() {
-	var ogRatio = document.getElementById("ratio").textContent;
+	var ogRatio = document.getElementById("ratio").textContent; //get ratio from DOM
 	console.log(ogRatio);
-	var lightRatio = parseInt(ogRatio) +1;
+	var lightRatio = parseInt(ogRatio) +1; //increase ratio by 1
 	console.log(lightRatio);
-	document.getElementById("ratio").textContent = lightRatio;
-	calculateBrew(); 
+	document.getElementById("ratio").textContent = lightRatio; //write new ratio to DOM
+	calculateBrew(); //recalculate brew using new ratio value
 }
 
+//Function to decrease ("tighten") brew ratio
 function makeStronger() {
-	var ogRatio = document.getElementById("ratio").textContent;
+	var ogRatio = document.getElementById("ratio").textContent; //get ratio from DOM
 	console.log(ogRatio);
-	var strongRatio = parseInt(ogRatio) -1;
+	var strongRatio = parseInt(ogRatio) -1; //decrement ratio
 	console.log(strongRatio);
-	document.getElementById("ratio").textContent = strongRatio;
-	calculateBrew(); 
+	document.getElementById("ratio").textContent = strongRatio; //write new ratio to DOM
+	calculateBrew(); //recalculate brew using new ratio value
 }
 
+//Function to clear input field once the user focuses
 function clearField() {
 	this.placeholder = "";
 }
+
+//Clear text input for recipe on focus
+nameField.addEventListener("focus", clearField);
 
 //Wire up "calculate brew" button
 brewButton.addEventListener("click", calculateBrew);
@@ -55,87 +62,53 @@ brewButton.addEventListener("click", calculateBrew);
 //Wire up "stronger" button
 strongerButton.addEventListener("click", makeStronger);
 
-//Wire up "weaker" button
+//Wire up "lighter" button
 lighterButton.addEventListener("click", makeLighter);
 
-//Clear text input for recipe on focus
-nameField.addEventListener("focus", clearField);
-
+//Wire up "save" button
 saveButton.addEventListener("click", createRecipe);
-// saveButton.addEventListener("click", showRecipe);
-// saveButton.addEventListener("click", writeSelect);
 
-// function Recipe(name, ounces, gramsCoffee, gramsWater, ratioWater) {
-// 	this.name = name;
-// 	this.ounces =  ounces;
-// 	this.gramsCoffee = gramsCoffee;
-// 	this.gramsWater = gramsWater;
-// 	this.ratioWater = ratioWater;
-// }
-
+//Function to save recipe in local storage
 function createRecipe() {
-	var recName = document.getElementById("nameInput").value;
-	var oz = recipeOutput[0];
+	var recName = document.getElementById("nameInput").value; //Grab user recipe name input
+	var oz = recipeOutput[0]; //Retrieve current recipe values from array
 	var ratio = recipeOutput[3];
-	var recipe = [oz, ratio]
-	localStorage.setItem(recName, JSON.stringify(recipe));
+	var recipe = [oz, ratio];
+	localStorage.setItem(recName, JSON.stringify(recipe)); //Store recipe name and values in local storage as key:value strings
 	console.log(recipe + " set to local storage");
-	var dropDown = document.getElementById("savedRec");
-	var newOption = document.createElement("option");
-	newOption.textContent = recName;
-	newOption.setAttribute("value", recName);
-	dropDown.appendChild(newOption);
+	var dropDown = document.getElementById("savedRec"); //Select dropdown
+	var newOption = document.createElement("option"); //Create new option
+	newOption.textContent = recName; //Assign recipe name to new option
+	newOption.setAttribute("value", recName); //Assign option value
+	dropDown.appendChild(newOption); //Add new option to dropdown
 }
 
-window.onLoad = createRecipeList();
-
+//Function to construct dropdown with previously saved recipes
 function createRecipeList() {
 	console.log("Onload select creation running!");
 	var nameIndex = localStorage.length;
-	for (i = 0; i < nameIndex; i++) {
-		var dropDown = document.getElementById("savedRec");
-		var newOption = document.createElement("option");
-		newOption.innerText = localStorage.key(i);
-		newOption.setAttribute("value", localStorage.key(i));
-		dropDown.appendChild(newOption);
+	for (i = 0; i < nameIndex; i++) { //Iterate through local storage
+		var dropDown = document.getElementById("savedRec"); //Select dropdown
+		var newOption = document.createElement("option"); //Create new option
+		newOption.innerText = localStorage.key(i);  //Assign option value with local storage .key
+		newOption.setAttribute("value", localStorage.key(i)); //Assign value with local storage .key
+		dropDown.appendChild(newOption); //Add new option to dropdown
 	}
 }
 
-//Function to repopulate DOM with saved recipe values
+//Builds the dropdown using saved recipes after page has loaded
+window.onLoad = createRecipeList();
+
+//Wire up dropdown menu to display saved recipes when selected
 selectDrop.addEventListener("change", savedRecipe);
 
+//Function to repopulate DOM with saved recipe values
 function savedRecipe() {
-	var name = selectDrop.value;
+	var name = selectDrop.value; //Grab selected option
 	console.log(name);
-	var recipe = JSON.parse(localStorage.getItem(name));
+	var recipe = JSON.parse(localStorage.getItem(name)); //Retrieve corresponding values from local storage
 	console.log(recipe);
-	document.getElementById("ozInput").value = parseInt(recipe[0]);
-	document.getElementById("ratio").textContent = recipe[1];
-	calculateBrew();
+	document.getElementById("ozInput").value = parseInt(recipe[0]); //Write oz value to DOM
+	document.getElementById("ratio").textContent = recipe[1]; //Write ratio to DOM
+	calculateBrew(); //Run calculate brew using saved recipe values
 }
-
-
-
-
-//Pre- refactored version of drop creation function
-//***************************
-// function createRecipeList() {
-// 	console.log("Onload select creation running!");
-// 	var nameIndex = localStorage.length;
-// 	var names = [];
-// 	for (i = 0; i < nameIndex; i++) {
-// 		names[i] = localStorage.key(i);
-// 	}
-// 	console.log(names);
-// 	for (i = 0; i < names.length; i++) {
-// 		var dropDown = document.getElementById("savedRec");
-// 		var newOption = document.createElement("option");
-// 		newOption.innerText = names[i];
-// 		newOption.setAttribute("value", names[i]);
-// 		dropDown.appendChild(newOption);
-// 	}
-// }
-
-
-
-
